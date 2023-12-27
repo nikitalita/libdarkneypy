415b261431fc009edecb4bc7fb9c8445cda7a922
nikitalita <69168929+nikitalita@users.noreply.github.com>
2023-12-26 17:18:17 -0800

    fix tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,6 @@
 
 def test_main():
-    return True
+    return None
     # m.init(bytes("/Users/nikita/Workspace/OCRAggregatorServer/data/model.cfg", encoding="utf-8"), bytes("/Users/nikita/Workspace/OCRAggregatorServer/data/model.weights", encoding="utf-8"), 0,1)
     # bbox_test
     # m.detect_mat(bytes([0,0,0,0]), 4, bbox_test)
